45e6fd4789c688dd2c864816054c2200ad4d9267
Prashant Singh <prashantsinghtariyal@gmail.com>
2023-05-03 21:04:35 +0530

    header component

---
+++ my-youtube/src/App.js
@@ -0,0 +1,17 @@
+
+import './App.css';
+import Header from './Header';
+import Body from './Body'
+import ButtonList from './ButtonList'
+import Sidebar from './Sidebar'
+
+function App() {
+  return (
+    <div className="App">
+    <Header/>
+    <Body/>
+     </div>
+  );
+}
+
+export default App;
