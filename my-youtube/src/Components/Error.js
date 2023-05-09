5cbc80d692a889c22ed1023cdec0cd0218fe0327
Prashant Singh <prashantsinghtariyal@gmail.com>
2023-05-09 21:20:36 +0530

    error handling

--- my-youtube/src/Components/Error.js
+++ my-youtube/src/Components/Error.js
@@ -1,9 +1,17 @@
 import React from 'react'
-
+import { useRouteError } from "react-router-dom";
 const Error = () => {
+    const err = useRouteError();
+      console.log(err);
   return (
-    <div>Fallback error!</div>
-  )
+    <div>
+      <h1>Oops!!</h1>
+      <h2>Something went wrong!!</h2>
+      <h2>{err.status + " :  " + err.statusText}</h2>
+    </div>
+  );
+  
 }
 
 export default Error
+
