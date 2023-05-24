3bbf7d3ad413794400f6bbdc7a27e3a5cec1a8e5
Prashant Singh <prashantsinghtariyal@gmail.com>
2023-05-24 20:30:02 +0530

    added comment add

--- my-youtube/src/Components/CommentList.js
+++ my-youtube/src/Components/CommentList.js
@@ -1,11 +1,53 @@
-import React from "react";
+import React, { useState } from "react";
 import Comment from "./Comment.js";
 import { commentsData } from "../utils/constants";
+import image from "../Assests/comment-icon.png";
+
+const CommentList = () => {
+  //const [commentInput, setCommentInput] = useState("");
+  const [CommentBtnState, setCommentBtnState] = useState(false);
+  return (
+    <>
+      <h1 className="text-2xl font-bold mt-2 ml-2">Comments : </h1>
+      <div className="flex flex-col mt-4">
+        <div className="flex gap-2">
+          <img src={image} alt="usericon" />
+          <input
+            type="text"
+            placeholder="Add a comment..."
+            className="border-b border-gray-200 dark:text-black md:p-2 focus:border-black text-xl focus:outline-none placeholder-stone-500  md:text-base w-full"
+            onChange={(e) => {
+              setCommentBtnState(true);
+            }}
+          />
+        </div>
+        <div className="ml-[4%] mt-[10px]">
+          <button className="md:text-sm md:px-4 md:py-2 px-2 py-1 text-[0.7rem] hover:bg-stone-200 font-semibold text-2xl rounded-full">
+            Cancel
+          </button>
+          <button
+            className={`md:text-sm md:px-4 md:py-2 px-2 py-1 text-[0.7rem] text-2xl font-semibold 
+          rounded-full${CommentBtnState ? "cursor-pointer bg-blue-600 text-white":"bg-stone-100 text-stone-500 dark:text-stone-500  cursor-not-allowed"}`}
+          >
+            Comment
+          </button>
+          {/* <button
+          className="md:text-sm md:px-4 md:py-2 px-2 py-1 text-[0.7rem] text-2xl font-semibold 
+          rounded-full bg-blue-600 text-white"
+        >
+          Comment
+        </button> */}
+        </div>
+      </div>
+      <CommentDataList commentsData={commentsData} />
+    </>
+  );
+};
 
 const CommentDataList = ({ commentsData }) => {
   return (
-    <> 
-      {commentsData.map((comment,index) => (
+    <>
+      {commentsData.map((comment, index) => (
         <div key={index}>
           <Comment comment={comment} />
           <div className="pl-5 border border-l-black ml-5">
@@ -13,15 +55,6 @@ const CommentDataList = ({ commentsData }) => {
           </div>
         </div>
       ))}
-    </>
-  );
-};
-
-const CommentList = () => {
-  return (
-    <>
-     <h1 className="text-2xl font-bold mt-2 ml-2">Comments : </h1>
-      <CommentDataList commentsData={commentsData} />
     </>
   );
 };
