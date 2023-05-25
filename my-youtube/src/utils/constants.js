eeeb27cef359c323c7e95effe5a81801fbf848f7
Prashant Singh <52065013+PrashantSinghT99@users.noreply.github.com>
2023-05-25 17:47:49 +0530

    Update constants.js

--- my-youtube/src/utils/constants.js
+++ my-youtube/src/utils/constants.js
@@ -1,4 +1,4 @@
-const API_KEY = "AIzaSyC6WbtdkOdvxwjHkVPp0Pv77l4fXhROeBg";
+const API_KEY = "";
 
 export const VIDEO_URL = "https://youtube.googleapis.com/youtube/v3/videos?part=snippet%2CcontentDetails%2Cstatistics&chart=mostPopular&maxResults=50&regionCode=IN&key=" + API_KEY;
 
