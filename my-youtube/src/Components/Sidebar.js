5eb75ef70cc14af28e9a62b1d929d7a262385453
Prashant Singh <prashantsinghtariyal@gmail.com>
2023-05-26 19:46:43 +0530

    added sidebar icons

--- my-youtube/src/Components/Sidebar.js
+++ my-youtube/src/Components/Sidebar.js
@@ -1,40 +1,38 @@
-import React from 'react'
-import { useSelector } from 'react-redux';
-import { Link } from 'react-router-dom'
+import React from "react";
+import { useSelector } from "react-redux";
+import { Link } from "react-router-dom";
+import { HiMusicNote } from "react-icons/hi";
+import { BsNewspaper } from "react-icons/bs";
+import { RxVideo } from "react-icons/rx";
+import {
+  MdHomeFilled,
+  MdSportsCricket,
+  MdTheaterComedy,
+  MdTravelExplore,
+  MdMovie,
+} from "react-icons/md";
 const Sidebar = () => {
-
-  const open = useSelector((store) => store.sideBarSlice.open)
+  const open = useSelector((store) => store.sideBarSlice.open);
 
   if (!open) return null;
   // p-5 shadow-lg w-48
   // pl-20 shadow-xl w-[210px] h-full text-lg mt-10
-  return <div className="p-5 shadow-lg w-48">
-    <ul>
-      <li>
-        <Link to='/'>Home</Link>
-      </li>
-      <li>
-        <h1>Demo</h1>
-      </li>
-      <li> Shorts</li>
-      <li> Videos</li>
-      <li> Live</li>
-    </ul>
-    <h1 className="font-bold pt-5">Subscriptions</h1>
-    <ul>
-      <li> Music</li>
-      <li> Sports</li>
-      <li> Gaming</li>
-      <li> Movies</li>
-    </ul>
-    <h1 className="font-bold pt-5">Watch Later</h1>
-    <ul>
-      <li> Music</li>
-      <li> Sports</li>
-      <li> Gaming</li>
-      <li> Movies</li>
-    </ul>
-  </div>
-}
+  return (
+    <div className="p-5 shadow-lg w-48 text-black text-center">
+      <ul>
+        <li><Link to="/"><MdHomeFilled></MdHomeFilled>Home</Link></li>
+        <li><HiMusicNote></HiMusicNote>Music</li>
+        <li><RxVideo></RxVideo>Shorts</li>
+        <li><BsNewspaper></BsNewspaper>News & Politics</li>
+        <li><MdSportsCricket></MdSportsCricket>Sports</li>
+        <li><MdTheaterComedy></MdTheaterComedy>Comedy</li>
+        <li><MdTravelExplore></MdTravelExplore>Travel & Events</li>
+        <li><MdMovie></MdMovie>Movies</li>
+      </ul>
+    </div>
+  );
+};
 
-export default Sidebar
+export default Sidebar;
+
+
--- my-youtube/src/utils/constants.js
+++ my-youtube/src/utils/constants.js
@@ -2,78 +2,78 @@ const API_KEY = "";
 
 export const VIDEO_URL = "https://youtube.googleapis.com/youtube/v3/videos?part=snippet%2CcontentDetails%2Cstatistics&chart=mostPopular&maxResults=50&regionCode=IN&key=" + API_KEY;
 
-export const MAX_LIVECHAT_COUNT=20;
+export const MAX_LIVECHAT_COUNT = 20;
+export const CATEGORY_ID_URL = "https://www.googleapis.com/youtube/v3/videoCategories?part=snippet&regionCode=IN&key=" + API_KEY
+export const VIDEO_SEARCH_API ="https://suggestqueries.google.com/complete/search?client=firefox&ds=yt&q=";
 
-export const VIDEO_SEARCH_API =
-  "https://suggestqueries.google.com/complete/search?client=firefox&ds=yt&q=";
+const nameList = [
+  'Time', 'Past', 'Future', 'Dev',
+  'Fly', 'Flying', 'Soar', 'Soaring', 'Power', 'Falling',
+  'Fall', 'Jump', 'Cliff', 'Mountain', 'Rend', 'Red', 'Blue',
+  'Green', 'Yellow', 'Gold', 'Demon', 'Demonic', 'Panda', 'Cat',
+  'Kitty', 'Kitten', 'Zero', 'Memory', 'Trooper', 'XX', 'Bandit',
+  'Fear', 'Light', 'Glow', 'Tread', 'Deep', 'Deeper', 'Deepest',
+  'Mine', 'Your', 'Worst', 'Enemy', 'Hostile', 'Force', 'Video',
+  'Game', 'Donkey', 'Mule', 'Colt', 'Cult', 'Cultist', 'Magnum',
+  'Gun', 'Assault', 'Recon', 'Trap', 'Trapper', 'Redeem', 'Code',
+  'Script', 'Writer', 'Near', 'Close', 'Open', 'Cube', 'Circle',
+  'Geo', 'Genome', 'Germ', 'Spaz', 'Shot', 'Echo', 'Beta', 'Alpha',
+  'Gamma', 'Omega', 'Seal', 'Squid', 'Money', 'Cash', 'Lord', 'King',
+  'Duke', 'Rest', 'Fire', 'Flame', 'Morrow', 'Break', 'Breaker', 'Numb',
+  'Ice', 'Cold', 'Rotten', 'Sick', 'Sickly', 'Janitor', 'Camel', 'Rooster',
+  'Sand', 'Desert', 'Dessert', 'Hurdle', 'Racer', 'Eraser', 'Erase', 'Big',
+  'Small', 'Short', 'Tall', 'Sith', 'Bounty', 'Hunter', 'Cracked', 'Broken',
+  'Sad', 'Happy', 'Joy', 'Joyful', 'Crimson', 'Destiny', 'Deceit', 'Lies',
+  'Lie', 'Honest', 'Destined', 'Bloxxer', 'Hawk', 'Eagle', 'Hawker', 'Walker',
+  'Zombie', 'Sarge', 'Capt', 'Captain', 'Punch', 'One', 'Two', 'Uno', 'Slice',
+  'Slash', 'Melt', 'Melted', 'Melting', 'Fell', 'Wolf', 'Hound',
+  'Legacy', 'Sharp', 'Dead', 'Mew', 'Chuckle', 'Bubba', 'Bubble', 'Sandwich', 'Smasher', 'Extreme', 'Multi', 'Universe', 'Ultimate', 'Death', 'Ready', 'Monkey', 'Elevator', 'Wrench', 'Grease', 'Head', 'Theme', 'Grand', 'Cool', 'Kid', 'Boy', 'Girl', 'Vortex', 'Paradox'
+];
 
-  const nameList = [
-    'Time', 'Past', 'Future', 'Dev',
-    'Fly', 'Flying', 'Soar', 'Soaring', 'Power', 'Falling',
-    'Fall', 'Jump', 'Cliff', 'Mountain', 'Rend', 'Red', 'Blue',
-    'Green', 'Yellow', 'Gold', 'Demon', 'Demonic', 'Panda', 'Cat',
-    'Kitty', 'Kitten', 'Zero', 'Memory', 'Trooper', 'XX', 'Bandit',
-    'Fear', 'Light', 'Glow', 'Tread', 'Deep', 'Deeper', 'Deepest',
-    'Mine', 'Your', 'Worst', 'Enemy', 'Hostile', 'Force', 'Video',
-    'Game', 'Donkey', 'Mule', 'Colt', 'Cult', 'Cultist', 'Magnum',
-    'Gun', 'Assault', 'Recon', 'Trap', 'Trapper', 'Redeem', 'Code',
-    'Script', 'Writer', 'Near', 'Close', 'Open', 'Cube', 'Circle',
-    'Geo', 'Genome', 'Germ', 'Spaz', 'Shot', 'Echo', 'Beta', 'Alpha',
-    'Gamma', 'Omega', 'Seal', 'Squid', 'Money', 'Cash', 'Lord', 'King',
-    'Duke', 'Rest', 'Fire', 'Flame', 'Morrow', 'Break', 'Breaker', 'Numb',
-    'Ice', 'Cold', 'Rotten', 'Sick', 'Sickly', 'Janitor', 'Camel', 'Rooster',
-    'Sand', 'Desert', 'Dessert', 'Hurdle', 'Racer', 'Eraser', 'Erase', 'Big',
-    'Small', 'Short', 'Tall', 'Sith', 'Bounty', 'Hunter', 'Cracked', 'Broken',
-    'Sad', 'Happy', 'Joy', 'Joyful', 'Crimson', 'Destiny', 'Deceit', 'Lies',
-    'Lie', 'Honest', 'Destined', 'Bloxxer', 'Hawk', 'Eagle', 'Hawker', 'Walker',
-    'Zombie', 'Sarge', 'Capt', 'Captain', 'Punch', 'One', 'Two', 'Uno', 'Slice',
-    'Slash', 'Melt', 'Melted', 'Melting', 'Fell', 'Wolf', 'Hound',
-    'Legacy', 'Sharp', 'Dead', 'Mew', 'Chuckle', 'Bubba', 'Bubble', 'Sandwich', 'Smasher', 'Extreme', 'Multi', 'Universe', 'Ultimate', 'Death', 'Ready', 'Monkey', 'Elevator', 'Wrench', 'Grease', 'Head', 'Theme', 'Grand', 'Cool', 'Kid', 'Boy', 'Girl', 'Vortex', 'Paradox'
-  ];
-  
-  export const generateRandomName = () => {
-    return nameList[Math.floor(Math.random() * nameList.length)];
-  };
-  
+export const generateRandomName = () => {
+  return nameList[Math.floor(Math.random() * nameList.length)];
+};
+
 export const commentsData = [
   {
-    id:1,
+    id: 1,
     name: generateRandomName(),
     text: "Great content, really enjoying the stream!",
     replies: [],
   },
   {
-    id:2,
+    id: 2,
     name: generateRandomName(),
     text: "Thanks for sharing this information, it's really important",
     replies: [
       {
-        id:3,
+        id: 3,
         name: generateRandomName(),
         text: "I'm blown away by how informative this stream is, thank you!",
         replies: [],
       },
       {
-        id:4,
+        id: 4,
         name: generateRandomName(),
         text: "I can't believe how much I'm learning from this",
         replies: [
           {
-            id:5,
+            id: 5,
             name: generateRandomName(),
             text: "I'm really impressed by the quality of this content, thank you",
             replies: [
-              {id:6,
+              {
+                id: 6,
                 name: generateRandomName(),
                 text: "I'm loving the positive energy of this stream",
                 replies: [
                   {
-                    id:7,
+                    id: 7,
                     name: generateRandomName(),
                     text: "This is amazing!",
                     replies: [
                       {
-                        id:8,
+                        id: 8,
                         name: generateRandomName(),
                         text: "This is exactly the kind of content I was hoping",
                         replies: [],
@@ -81,7 +81,7 @@ export const commentsData = [
                     ],
                   },
                   {
-                    id:9,
+                    id: 9,
                     name: generateRandomName(),
                     text: "This is amazing!",
                     replies: [],
@@ -94,7 +94,8 @@ export const commentsData = [
       },
     ],
   },
-  {  id:10,
+  {
+    id: 10,
     name: generateRandomName(),
     text: "This is exactly the kind of content I was hoping",
     replies: [],
